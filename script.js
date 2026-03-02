43c5dce7256165c37dce4fec7223b9c6a32c645b
Caio-RB <caiorichate@proton.me>
2026-03-01 21:18:22 -0300

    code: adicionando funcionalidade de registro de laps

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const clock = document.querySelector("#clock");
 const start = document.querySelector(".start");
 const restart = document.querySelector(".restart");
 const stop = document.querySelector(".stop");
+const lap = document.querySelector(".lap");
+const laps = document.querySelector(".laps");
+const lapsList = document.querySelector(".laps-list");
+stop.style.display="none";
 let data = new Date();
 data.setHours(00,00,00);
 let timer;
@@ -12,21 +16,38 @@ function showtime(){
 
 function startTimer(){
 	if(timer) return;
+	start.style.display = "none";
+	stop.style.display = "";
 	timer = setInterval(()=>{
-		clock.innerText = showtime();
 		data.setSeconds(data.getSeconds() + 1);
+		clock.innerText = showtime();
 	},1000);
 }
 
 function resetTimer(){
 	data.setHours(00,00,00);
 	clock.innerText = showtime();
+	if(start.display != "none"){
+		start.innerText = "Start";
+	}
 }
 
 function stopTimer(){
+	start.style.display = "";
+	start.innerText = "Resume"
+	stop.style.display = "none";
 	clearInterval(timer);
 	timer = null;
 }
+
+function lapTimer(){
+	let newLap = document.createElement("li");
+	newLap.innerText = showtime();
+	newLap.classList.add("lap-register");
+	lapsList.prepend(newLap);
+}
+
 start.addEventListener("click", startTimer);
 restart.addEventListener("click", resetTimer);
 stop.addEventListener("click", stopTimer);
+lap.addEventListener("click", lapTimer);
